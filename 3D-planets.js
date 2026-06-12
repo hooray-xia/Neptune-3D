13bb79d49d038207625051f3792bba4b14420dff
ThatCodingFrog <aiden.schuneman@outlook.com>
2026-06-12 16:51:23 -0600

    Create 3D-planets.js for future 3D assets

---
+++ 3D-planets.js
@@ -0,0 +1,9 @@
+//Future home for all 3D stuff to be imported to KA
+
+const urls = {};
+
+var textures = [];
+
+function loadTextures() {
+  //Loop through urls, add a new texture loader and load the URL, then put it into the textures array/map
+}
